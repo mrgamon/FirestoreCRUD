17e38a86f203e3633e2d380cc0ce13f98cfa4418
brixNessia <brixaissen@gmail.com>
2018-01-11 22:52:26 +0800

    additional commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule
