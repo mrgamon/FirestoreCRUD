0e2c10e5ad51891a47f9026d9b34cd33a339bd94
brixNessia <brixaissen@gmail.com>
2018-01-11 22:35:47 +0800

    added firebase code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB-833_wI6Ha6ucYhMynmU7Uu-LsKRT43E",
+      authDomain: "firestore-crud2.firebaseapp.com",
+      databaseURL: "https://firestore-crud2.firebaseio.com",
+      projectId: "firestore-crud2",
+      storageBucket: "firestore-crud2.appspot.com",
+      messagingSenderId: "1009297380603"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,4 +5,10 @@
 
 export const environment = {
   production: false
+  apiKey: "AIzaSyB-833_wI6Ha6ucYhMynmU7Uu-LsKRT43E",
+    authDomain: "firestore-crud2.firebaseapp.com",
+    databaseURL: "https://firestore-crud2.firebaseio.com",
+    projectId: "firestore-crud2",
+    storageBucket: "firestore-crud2.appspot.com",
+    messagingSenderId: "1009297380603"
 };
